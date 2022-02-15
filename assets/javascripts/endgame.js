d3adb41689e292b273254db83227486204621732
Dyoneus <jiayuan999@hotmail.com>
2022-02-15 18:11:56 +0800

    Create condition based on Gender for finalizing scene

---
+++ assets/javascripts/endgame.js
@@ -0,0 +1,6 @@
+if(localStorage.getItem("gender") == "male"){  
+    document.getElementById("human-image").src = "assets/images/maleBack.png";
+}
+else{
+    document.getElementById("human-image").src = "assets/images/femaleBack.png";
+}
