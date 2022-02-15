142109b18593f138119560379cbae9e5db46a25b
Dyoneus <jiayuan999@hotmail.com>
2022-02-15 18:18:10 +0800

    Add EventListener to redirect back to Battle scene for replayability

--- assets/javascripts/endgame.js
+++ assets/javascripts/endgame.js
@@ -4,3 +4,33 @@ if(localStorage.getItem("gender") == "male"){
 else{
     document.getElementById("human-image").src = "assets/images/femaleBack.png";
 }
+
+function getPokemonImage(number) {
+    return (
+          `https://raw.githubusercontent.com/PokeAPI/` +
+          `sprites/master/sprites/pokemon/${number}.png`
+    );
+}
+
+document.getElementById("bodyId").addEventListener("click", function(){
+    console.log("here")
+    if(localStorage.getItem("gender") == "male"){
+          window.location.href = "battlemale.html";
+    }
+    else{
+          window.location.href = "battlefemale.html";
+    }
+});
+
+function findGetParameter(parameterName) {
+    var result = null,
+          tmp = [];
+    location.search
+          .substr(1)
+          .split("&")
+          .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+          });
+    return result;
+}
